cbacd84af353623218a3883a38ed6074d1499bcb
ciaranSHZhou <94954162+ciaranSHZhou@users.noreply.github.com>
2023-02-07 02:38:36 +0000

    reorganized projects

---
+++ Mini-Projects/Week3/README.md
@@ -0,0 +1,6 @@
+### Steps to run
+
+* 'make format' to format code
+* 'make lint' to lint
+* 'make release arm' which is 'cargo lambda build --release --arm64'
+* 'make deploy' which is 'cargo lambda deploy'
--- Projects/Project1-Rust-CLI/.devcontainer/src/lib.rs
+++ Projects/Project1-Rust-CLI/.devcontainer/src/lib.rs
@@ -2,9 +2,7 @@
 pub fn quote_of_the_day(quote_type: &str) -> String {
     // create a switch statement to return the quote of the day
     let quote = match quote_type {
-        "motivational" => {
-            "The only peole who will know are the people who keep moving forward. - Eren Yeager"
-        }
+        "motivational" => "The only peole who will know are the people who keep moving forward. - Eren Yeager",
         "sad" => "We'll never amount to anything. Not you, not I. - Hawkwood the Deserter",
         "lie" => "You are awesome! - Who",
         _ => "No quote of the day",
